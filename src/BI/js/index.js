e7c248a80e6ee5e84447415ca1a91b6f21f45673
babyisun@qq.com <babyisun@qq.com>
2018-02-05 13:33:13 +0800

    complete

--- src/BI/js/index.js
+++ src/BI/js/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Link} from 'react-router-dom';
 // import {Router, Route, IndexRedirect, hashHistory, Link} from 'react-router';
-import "../../css/page/index.scss";
+import "../css/page/index.scss";
 
 class Home extends React.Component {
     render() {
         return (
-            <div className="main home">
+            <div className="container">
                 <h1 className="home-title">ReactEasyUI</h1>
-                <p className="home-desc">ReactEasyUI 是一个基于 React 的常用组件库，封装了日期、列表、图表等常见使用场景，由<Link to="about">易车前端团队</Link>倾力打造。</p>
+                <p className="form1">ReactEasyUI 是一个基于 React 的常用组件库，封装了日期、列表、图表等常见使用场景，由<Link to="about">易车前端团队</Link>倾力打造。</p>
                 <div className="home-btns">
                     <Link className="btn btn-start" to="getting-started">快速开始</Link>
                     <a
