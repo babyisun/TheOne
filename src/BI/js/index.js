25d48c4a7b28cecbdaeb244b48138c38015b2eb6
yishen <yishen@cig.com.cn>
2018-02-06 10:57:47 +0800

    update index

--- src/BI/js/index.js
+++ src/BI/js/index.js
@@ -1,22 +1,25 @@
+// import "./lib/lib.js"
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter, Link} from 'react-router-dom';
 // import {Router, Route, IndexRedirect, hashHistory, Link} from 'react-router';
 import "../css/page/index.scss";
+// import "https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.0/jquery-ui.js";
+import Header from "./component/Header.js";
+import Editor from "./component/Editor.js";
+import Explorer from "./component/Explorer.js";
+import Pad from "./component/Pad.js";
+// import "./component/two2.js"
 
 class Home extends React.Component {
+
     render() {
         return (
-            <div className="container">
-                <h1 className="home-title">ReactEasyUI</h1>
-                <p className="form1">ReactEasyUI 是一个基于 React 的常用组件库，封装了日期、列表、图表等常见使用场景，由<Link to="about">易车前端团队</Link>倾力打造。</p>
-                <div className="home-btns">
-                    <Link className="btn btn-start" to="getting-started">快速开始</Link>
-                    <a
-                        className="btn btn-github"
-                        href="https://github.com/reacteasyui/ReactEasyUI"
-                        target="_blank">GitHub</a>
-                </div>
+            <div>
+                <Header/>
+                <Editor/>
+                <Explorer/>
+                <Pad/>
             </div>
         );
     }
