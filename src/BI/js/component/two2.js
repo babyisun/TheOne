c77167152deb1282970a4f72ac3fed444972d84a
yishen <yishen@cig.com.cn>
2018-02-07 09:55:43 +0800

    upload

--- src/BI/js/component/two2.js
+++ src/BI/js/component/two2.js
@@ -1,12 +1,137 @@
-$(function(){
+$(function () {
 
     initial();
-    function initial(){
+
+    function initial() {
         var screenH = $(window).height(),
-        screenW = $(window).width(),
-        barLeft = $('.bar-left'),
-        barRight = $('.bar-right');
-        barLeft.css('height',screenH-40+'px');
-        barRight.css('height',screenH-50+'px');
+            screenW = $(window).width(),
+            barLeft = $('.bar-left'),
+            barRight = $('.bar-right');
+        barLeft.css('height', screenH - 40 + 'px');
+        barRight.css('height', screenH - 50 + 'px');
     }
 })
+
+$(function () {
+    var options = {
+        width: 12,
+        float: false,
+        removable: '.trash',
+        removeTimeout: 100,
+        acceptWidgets: '.grid-stack-item'
+    };
+    // $('#grid1').gridstack(options);
+    $('#grid2').gridstack(_.defaults({
+        float: true
+    }, options));
+
+    var items = [{
+            x: 0,
+            y: 0,
+            width: 2,
+            height: 2
+        },
+        {
+            x: 3,
+            y: 1,
+            width: 1,
+            height: 2
+        },
+        {
+            x: 4,
+            y: 1,
+            width: 1,
+            height: 1
+        },
+        {
+            x: 2,
+            y: 3,
+            width: 3,
+            height: 1
+        },
+        {
+            x: 2,
+            y: 5,
+            width: 1,
+            height: 1
+        }
+    ];
+
+    $('.grid-stack').each(function () {
+        var grid = $(this).data('gridstack');
+
+        _.each(items, function (node) {
+            grid.addWidget($('<div><div class="grid-stack-item-content" /></div>'),
+                node.x, node.y, node.width, node.height);
+        }, this);
+    });
+
+    $('.sidebar .grid-stack-item').draggable({
+        revert: 'invalid',
+        handle: '.grid-stack-item-content',
+        scroll: false,
+        appendTo: 'body'
+    });
+
+    $('.container-fluid').on("click", ".grid-stack-item-content", function () {
+        var distance = ($(this).offset().top) - ($(this).parents('.grid-stack').offset().top);
+        if (distance < 42) {
+            var _left = Math.round($(this).offset().left + 1),
+                _top = Math.round($(this).offset().top + $(this)[0].offsetHeight);
+        } else {
+            var _left = Math.round($(this).offset().left + 1),
+                _top = Math.round($(this).offset().top - 42);
+        }
+        var _editor = $('.grid-stack-control')
+
+        if (_editor.length == 0) {
+            $('.container-fluid').append(
+                '<div class="grid-stack-control" style="display: block; left: ' + _left +
+                'px; top: ' + _top + 'px;">' +
+                '<div class="grid-stack-control-title"><span>yourTitle<span></div>' +
+                '<div class="grid-stack-control-bar">' +
+                '<span class="iconfont icon-infomation" /><span class="iconfont icon-infomation" /><span class="iconfont icon-infomation" /><span class="split" /><span class="state">State: X</span><span class="split" /><span class="toll"><span class="iconfont icon-title selected" /><span class="iconfont icon-down" /><span class="iconfont icon-table select select-hide" /><span class="iconfont icon-line-chart select select-hide" /><span class="iconfont icon-bar-chart select select-hide" /></span>' +
+                '</div>' +
+                '</div>'
+            );
+        } else if (_editor.css('display') == 'block' && _editor.css('left') == (_left + 'px') &&
+            _editor.css('top') == (_top + 'px')) {
+            _editor.css('display', 'none');
+        } else {
+            _editor.css('display', 'block');
+        }
+        _editor.css('left', _left + 'px');
+        _editor.css('top', _top + 'px');
+
+    });
+    $('.container-fluid').on({
+        mousedown: function () {
+            var _editor = $('.grid-stack-control');
+            $(document).on('mousemove.drag', function (e) {
+                _editor.css('display', 'none');
+            });
+        },
+        mouseup: function () {
+            $(document).off('mousemove.drag');
+        }
+    })
+
+    $('.container-fluid').on("click", ".toll", function () {
+        var _select = $(this).children('.select');
+        if (_select.hasClass('select-hide')) {
+            _select.removeClass('select-hide');
+        } else {
+            _select.addClass('select-hide');
+        }
+    });
+
+    $(document).click(function (e) {
+        var _editor = $('.grid-stack-control');
+        var a = $(e.target).closest(".grid-stack-item-content").length,
+            b = $(e.target).parents(".grid-stack-control").length;
+        if (!a && !b) {
+            _editor.css('display', 'none');
+        }
+    });
+
+});
--- src/BI/js/index.js
+++ src/BI/js/index.js
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter, Link} from 'react-router-dom';
 // import {Router, Route, IndexRedirect, hashHistory, Link} from 'react-router';
 import "../css/page/index.scss";
+import "../css/font/iconfont.css"
 import Header from "./component/Header.js";
 import Editor from "./component/Editor.js";
 import Explorer from "./component/Explorer.js";
 import Pad from "./component/Pad.js";
 import "./component/two2.js";
-// import "./plugin/gridstack.js";
+
+import "./plugin/lodash.min.js";
+import "./plugin/gridstack.js";
 import "./plugin/gridstack.jQueryUI.js";
+// import '../css/font/iconfont.js'
 
 
 class Home extends React.Component {
@@ -24,6 +28,8 @@ class Home extends React.Component {
             </div>
         );
     }
+
+
 }
 
 ReactDOM.render((
