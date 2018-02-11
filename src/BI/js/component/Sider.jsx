4e0e7cba10a6c63fafb26866a15427cf691d31fc
babyisun@qq.com <babyisun@qq.com>
2018-02-11 11:38:13 +0800

    update sider and editor

---
+++ src/BI/js/component/Sider.jsx
@@ -0,0 +1,126 @@
+import React from "react";
+import {Link, Route} from 'react-router-dom';
+import "../plugin/scrollbar";
+import "../plugin/gotop";
+
+export default class Sider extends React.Component {
+    constructor(props) {
+        super(props);
+        this.menu = [
+            {
+                name: "快速开始",
+                class: "btn-hide",
+                url: "/getting-started"
+            },
+            {
+                name: "基础组件",
+                url: "/base",
+                son: [
+                    {name: "AutoComplete", url: "/auto-complete"},
+                    {name: "FreeCheckBox", url: "/free-check-box"},
+                    {name: "TextWithCount", url: "/text-with-count"},
+                    {name: "DelIconInput", url: "/del-icon-input"},
+                    {name: "SwitchBtn", url: "/switch-btn"},
+                    {name: "Counter", url: "/counter"},
+                    {name: "Element", url: "/element"}
+                ]
+            },
+            {
+                name: "日期与时间组件",
+                url: "/date",
+                son: [
+                    {name: "DateRangePicker", url: "/date-range-picker", icon: "pc"},
+                    {name: "DateChooser", url: "/date-chooser", icon: "pc"},
+                    {name: "Calender", url: "/calender"},
+                    {name: "AppCalender", url: "/app-calender", icon: "mobile"},
+                    {name: "Schedule", url: "/schedule"},
+                    {name: "Timer", url: "/timer"}
+                ]
+            },
+            {
+                name: "列表组件",
+                url: "/list",
+                son: [
+                    {name: "Table", url: "/table", icon: "pc"}
+                ]
+            },
+            {
+                name: "图表组件",
+                url: "/echarts",
+                class: "btn-hide",
+                son: [
+                    {name: "Pie", url: "/pie"},
+                    {name: "Gauge", url: "/gauge"},
+                    {name: "PieAndLineChart", url: "/pie-and-line-chart"},
+                    {name: "PieWithLabel", url: "/pie-with-label"},
+                    {name: "Scatter", url: "/scatter"},
+                    {name: "Line", url: "/line"},
+                    {name: "SimpleBar", url: "/simple-bar"},
+                    {name: "VerticalBar", url: "/vertical-bar"},
+                    {name: "HorizontalBar", url: "/horizontal-bar"},
+                    {name: "MultipleBar", url: "/multiple-bar"},
+                    {name: "LineAndPopupChart", url: "/line-and-popup-chart"},
+                    {name: "MapChart", url: "/map-chart"},
+                    {name: "DoubleYLineChart", url: "/double-y-line-chart"}
+                ]
+            },
+            {
+                name: "进度与比例图组件",
+                url: "/progress",
+                son: [
+                    {name: "ProgressChart", url: "/progress-chart"},
+                    {name: "ProgressBar", url: "/progress-bar"},
+                    {name: "Compare", url: "/compare"},
+                    {name: "Ring", url: "/ring"},
+                    {name: "PieChart", url: "/pie-chart"},
+                    {name: "Loading", url: "/loading"},
+                    {name: "Progress", url: "/progress"},
+                    {name: "Venn", url: "/venn"},
+                    {name: "Overlap", url: "/overlap"}
+                ]
+            },
+            {
+                name: "其它组件",
+                url: "/others",
+                son: [
+                    {name: "AudioPlay", url: "/audio-play"},
+                    {name: "IframeLoad", url: "/iframe-load"}
+                ]
+            },
+        ];
+    }
+
+    render() {
+        return (
+            <div className="sider">
+                {
+                    this.menu.map((item, i) => {
+                        return (
+                            <div key={i}>
+                                <Link to={item.url}
+                                      className={`primary ${item.son ? 'btn-toggle' : ''} ${item.class ? item.class : ''}`}
+                                      activeClassName="active"
+                                >{item.name}</Link>
+                                <div className={`toggle-list`}>
+                                    {
+                                        item.son && item.son.map((r, j)=> {
+                                            return (
+                                                <Link key={j} to={item.url+r.url} activeClassName="active">
+                                                    {r.name}
+                                                    {r.icon ? <i className={`re-site-icon re-site-icon-${r.icon}`}></i> : ''}
+                                                </Link>);
+                                        })
+                                    }
+                                </div>
+                            </div>
+                        );
+                    })
+                }
+            </div>
+        );
+    }
+
+    componentDidMount() {
+        $('.sider').scrollbar();
+    }
+}
--- src/BI/js/index.js
+++ src/BI/js/index.js
@@ -1,41 +1,59 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter, Link, Redirect, Route,HashRouter,history} from 'react-router-dom';
+import {
+    BrowserRouter,
+    Link,
+    Redirect,
+    Route,
+    HashRouter,
+    history
+} from 'react-router-dom';
 //import {Router, Route, IndexRedirect, hashHistory} from 'react-router';
 import "../css/page/index.scss";
 import "../css/font/iconfont.css";
 import Header from "./component/Header.jsx";
+import Sider from "./component/Sider.jsx";
 import Editor from "./component/Editor.jsx";
 import Explorer from "./component/Explorer.jsx";
 import Pad from "./component/Pad.jsx";
+
 
 //import "./plugin/lodash.min.js";
 import "./plugin/gridstack.js";
 import "./plugin/gridstack.jQueryUI.js";
 // import '../css/font/iconfont.js'
 
-class Home extends React.Component {
+class App extends React.Component {
 
     render() {
         return (
             <div>
                 <Header/>
-                <Editor/>
-                <Explorer/> {this.props.children}
-                {/* <Pad/>  */}
+                <div className="main clearfix">
+                    <Sider/>
+                    <div className="docs">
+                        <div className="content">
+                            {this.props.children}
+                        </div>
+                    </div>
+                    <Editor/>
+                </div>
+                {/* <Editor/>
+                <Explorer/>
+                <Pad/>  */}
             </div>
         );
     }
 
     componentDidMount() {
         //initial position
-        var screenH = $(window).height(),
-        screenW = $(window).width(),
-        barLeft = $('.bar-left'),
-        barRight = $('.bar-right');
-        barLeft.css('height', screenH - 40 + 'px');
-        barRight.css('height', screenH - 50 + 'px');
-        
+        // var screenH = $(window).height(),
+        //     screenW = $(window).width(),
+        //     barLeft = $('.bar-left'),
+        //     barRight = $('.bar-right');
+        // barLeft.css('height', screenH - 40 + 'px');
+        // barRight.css('height', screenH - 50 + 'px');
+
     }
 
 }
@@ -43,7 +61,7 @@ class Home extends React.Component {
 ReactDOM.render((
     <HashRouter history={history}>
         <div>
-            <Route path="/" component={Home}/>
+            <Route path="/" component={App}/>
             <Route path="/pad" component={Pad}/>
         </div>
     </HashRouter>
