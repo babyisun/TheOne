dc9b0e1bc4b8d5068fb5b24c33fddfde0572e5b4
babyisun@qq.com <babyisun@qq.com>
2018-02-12 16:34:03 +0800

    change model

--- src/BI/sever/database.js
+++ src/BI/sever/database.js
@@ -1,11 +1,10 @@
-
 import Sequelize from 'sequelize'
 import config from './config'
 
 var sequelize = new Sequelize(config.database, config.username, config.password, {
     host: config.host,
     dialect: 'mysql',
-
+    port: config.port,
     pool: {
         max: 5,
         min: 0,
@@ -16,12 +15,6 @@ var sequelize = new Sequelize(config.database, config.username, config.password,
 
 module.exports = sequelize
 
-// sequelize.sync()
-//     .then(() => User.create({
-//         UserID: '123456',
-//         Mobile: '134533452434',
-//         Password: '1232143',
-//         Role: 'admin',
-//         AddTime: '2018.2.11',
-//         Status: 'publish'
-//     }))
+// sequelize.sync()     .then(() => User.create({         UserID: '123456',
+//    Mobile: '134533452434',         Password: '1232143',         Role:
+// 'admin',         AddTime: '2018.2.11',         Status: 'publish'     }))
--- src/BI/sever/model/User.js
+++ src/BI/sever/model/User.js
@@ -2,46 +2,44 @@ import Sequelize from 'sequelize'
 import sequelize from '../database'
 import bcrypt from 'bcryptjs'
 
-var User = sequelize.define(
-    'user',{
-        'UserName' : {
-            'type' : Sequelize.STRING,
-            'initialAutoIncrement ': true
-        },
-        'Mobile' : {
-            'type' : Sequelize.STRING,
-            'allowNull' : false
-        },
-        'Password' : {
-            'type': Sequelize.STRING,
-            'allowNull': false,
-        },
-        'Role' : {
-            'type' : Sequelize.INTEGER,
-            'allowNull': false
-        },
-        'Status' : {
-            'type' : Sequelize.INTEGER
-        }
+var User = sequelize.define('user', {
+    'UserName': {
+        'type': Sequelize.STRING,
+        'initialAutoIncrement ': true
     },
-    {
-        instanceMethods: {
-            generateHash(Password) {
-                return bcrypt.hash(Password, bcrypt.genSaltSync(8));
-            },
-            validPassword(Password) {
-                return bcrypt.compare(Password, this.password);
-            }
+    'Mobile': {
+        'type': Sequelize.STRING,
+        'allowNull': false
+    },
+    'Password': {
+        'type': Sequelize.STRING,
+        'allowNull': false
+    },
+    'Role': {
+        'type': Sequelize.INTEGER,
+        'allowNull': false
+    },
+    'Status': {
+        'type': Sequelize.INTEGER
+    }
+}, {
+    instanceMethods: {
+        generateHash(Password) {
+            return bcrypt.hash(Password, bcrypt.genSaltSync(8));
+        },
+        validPassword(Password) {
+            return bcrypt.compare(Password, this.password);
         }
     }
-)
+})
+/*
+sequelize.sync()
+    .then(() => User.create({
+        UserName: '1',
+        Mobile: '123214343',
+        Password: '2',
+        Role: 1,
+        Status: 3
+    })) */
 
-// sequelize.sync()
-//     .then(() => User.create({
-//         UserName: '1',
-//         Mobile: '123214343',
-//         Password: '2',
-//         Role: 1,
-//         Status: 3
-//     }))
 module.exports = User
