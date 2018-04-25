aaf0f305f0c87915b9873f44176e3861ee475761
babyisun@qq.com <babyisun@qq.com>
2018-04-25 16:08:33 +0800

    add note

--- src/BI/js/store/MyStore.js
+++ src/BI/js/store/MyStore.js
@@ -1,15 +1,18 @@
 import {observable, computed, action} from "mobx";
 
 class MyStore {
-    // constructor() {
-    //     this.number = 10;
-    // }
-
+    constructor() {
+        //this.number = 10;
+    }
+    // 声明
     @observable number = 10;
 
+    //自计算属性
     @computed get getNumberLength() {
         return ("" + this.number).length;
     }
+    
+    //方法
     @action setNumber(value) {
         this.number += value;
     }
